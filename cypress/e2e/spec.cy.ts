7c59aea5b258a640a7d156dd59bea210c6ae4f24
lgrignon <louis.grignon@protonmail.com>
2023-05-15 12:21:40 +0200

    initial add

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,49 @@
+
+const ODYSSEY_USERNAME = "louis.grignon@wildcodeschool.com"; 
+const ODYSSEY_PASS = "??????"; 
+
+function loginToOdyssey() {
+  cy.visit('https://odyssey.wildcodeschool.com')
+  cy.get('[class$="formInputs"] input[name="email"]').type(ODYSSEY_USERNAME);
+  cy.get('[class$="formInputs"] input[name="password"]').type(ODYSSEY_PASS);
+  cy.contains("Je me connecte").click() 
+
+  // wait for login to complete, and left menu to be visible
+  cy.contains('Dashboard', { timeout: 10000 }).should('be.visible');
+}
+
+function openOdysseyPageFromMenu(itemName: string) {
+  return cy.get('[role="button"]').contains(itemName).click()
+}
+
+describe.only('Search quest on Odyssey', () => {
+  it('Search quest in Odyssey', () => {
+    loginToOdyssey();
+
+    cy.visit('https://odyssey.wildcodeschool.com/crews/558')
+    cy.get('[src="https://www.gravatar.com/avatar/57272?d=monsterid&s=300"]').click()
+    cy.contains("Masquerade").click()
+    
+    openOdysseyPageFromMenu("Quêtes");
+    
+    cy.get('[class$="searchField"] input[type="text"]').type('Angular');
+    cy.get('button[class$="searchButton"]').click();    
+
+    cy.contains('Voir la quête').click()
+    // assert that opened page has correct content
+    cy.contains(`Introduction`, { timeout: 10000 }).should('be.visible');
+  })
+  it('Search coworker in Odyssey', () => {
+    loginToOdyssey();
+
+    cy.visit('https://odyssey.wildcodeschool.com/crews/558')
+    cy.get('[src="https://www.gravatar.com/avatar/57272?d=monsterid&s=300"]').click()
+    cy.contains("Masquerade").click()
+    
+    openOdysseyPageFromMenu("Ma Promo");
+    cy.contains("William Bouet").parent('[class$="userContent"]').prev().click()
+
+    // assert that opened page has correct content
+    cy.contains(`Formation développeur d'applications en alternance`, { timeout: 10000 }).should('be.visible');
+  })
+})
